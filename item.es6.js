93d6397302cc1c43ed90e4a11a818538866825d8
Aaron Shafovaloff <aaronshaf@gmail.com>
2015-04-13 00:46:30 -0600

    Only drag items with handle

--- item.es6.js
+++ item.es6.js
@@ -4,13 +4,35 @@ import DragHandle from './drag-handle.es6'
 import classSet from 'class-set'
 
 const ItemGroupItem = React.createClass({
+  displayName: 'ItemGroupItem',
+
   propTypes: {
     className: React.PropTypes.string
   },
 
-  displayName: 'ItemGroupItem',
+  getInitialState () {
+    return {
+      isDragHandleGrabbed: false
+    }
+  },
 
-  render() {
+  handleDragStart (event) {
+    if(!this.state.isDragHandleGrabbed) {
+      event.preventDefault()
+      return false
+    }
+    if(this.props.handleDragStart) {
+      this.props.handleDragStart(event)
+    }
+  },
+
+  handleMouseDown (event) {
+    const handleElement = React.findDOMNode(this.refs.handle)
+    const isDragHandleGrabbed = handleElement.contains(event.target)
+    this.setState({isDragHandleGrabbed})
+  },
+
+  render () {
     const classes = classSet(this.props.className, 'ReactSortableItemGroups__item')
 
     return (
@@ -19,11 +41,11 @@ const ItemGroupItem = React.createClass({
           type={this.props.type}
           data={this.props.data}
           handleDrop={this.props.handleDrop}
-          handleDragStart={this.props.handleDragStart}
+          handleDragStart={this.handleDragStart}
           handleDragEnd={this.props.handleDragEnd}
           handleAcceptTest={this.props.handleAcceptTest}>
-        <li>
-          <DragHandle />
+        <li onMouseDown={this.handleMouseDown}>
+          <DragHandle ref="handle" />
           <div className="ReactSortableItemGroups__item-content">
             {this.props.children}
           </div>
